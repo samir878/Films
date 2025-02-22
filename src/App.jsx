f4e5d924ce14f37badc0bc2a50b1b6a11ddc9b95
samir messaoudi <samirmessaoudi04@gmail.com>
2025-02-22 18:22:42 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const Card=()=>{
+  
+  return(
+     <h2>hey</h2>
+  )
+  
+}
+
+export default App
